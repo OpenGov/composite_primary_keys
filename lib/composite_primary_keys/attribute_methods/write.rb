eba46ad901fd5475930584e283d9590e88b5778b
Loren Abrams <labrams@opengov.com>
2014-04-01 22:32:19 -0700

    Only set the highest resolution field of an objects foreign key on the destination object

--- lib/composite_primary_keys/attribute_methods/write.rb
+++ lib/composite_primary_keys/attribute_methods/write.rb
@@ -8,25 +8,35 @@ module ActiveRecord
           unless value.length == attr_name.length
             raise "Number of attr_names and values do not match"
           end
-          [attr_name, value].transpose.map {|name,val| write_attribute(name, val)}
-          value
-        else
-          attr_name = attr_name.to_s
-          # CPK
-          # attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key
-          attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key && !self.composite?
-          @attributes_cache.delete(attr_name)
-          column = column_for_attribute(attr_name)
 
-          unless column || @attributes.has_key?(attr_name)
-            ActiveSupport::Deprecation.warn(
-                "You're trying to create an attribute `#{attr_name}'. Writing arbitrary " \
-                "attributes on a model is deprecated. Please just use `attr_writer` etc."
-            )
+          attr_name[0..-2].each_with_index do |prop, index|
+            prop_value = value[index]
+            unless prop_value.nil? || prop_value == read_attribute(prop)
+              raise "Assignment would augment the object's primary key"
+            end
           end
-          @attributes[attr_name] = type_cast_attribute_for_write(column, value)
+
+          attr_name = attr_name[-1]
+          value = value[-1]
         end
+
+        attr_name = attr_name.to_s
+        # CPK
+        # attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key
+        attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key && !self.composite?
+        @attributes_cache.delete(attr_name)
+        column = column_for_attribute(attr_name)
+
+        unless column || @attributes.has_key?(attr_name)
+          ActiveSupport::Deprecation.warn(
+              "You're trying to create an attribute `#{attr_name}'. Writing arbitrary " \
+              "attributes on a model is deprecated. Please just use `attr_writer` etc."
+          )
+        end
+
+        @attributes[attr_name] = type_cast_attribute_for_write(column, value)
       end
+
       alias_method :raw_write_attribute, :write_attribute
     end
   end
