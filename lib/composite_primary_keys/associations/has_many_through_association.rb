987d6ef6d601e28440e938e67ab16c45e015b1e3
Loren Abrams <labrams@opengov.com>
2014-04-07 09:34:27 -0700

    Fix through record field extractions for composite keys

--- lib/composite_primary_keys/associations/has_many_through_association.rb
+++ lib/composite_primary_keys/associations/has_many_through_association.rb
@@ -1,17 +1,19 @@
 module ActiveRecord
   module Associations
     class HasManyThroughAssociation
+      def construct_join_fields
+        [Array(source_reflection.foreign_key), Array(source_reflection.association_primary_key(reflection.klass))]
+      end
+
       def construct_join_attributes(*records)
         ensure_mutable
 
-        association_fk = Array(source_reflection.foreign_key)
-        association_pk = Array(source_reflection.association_primary_key(reflection.klass))
-
-        join_fields = association_fk.zip(association_pk)
+        association_fk, association_pk = construct_join_fields
+        paired_join_fields = association_fk.zip(association_pk)
 
         table = Arel::Table.new(through_association.scoped.table_name)
         and_conditions = records.map do |record|
-          eq_conditions = join_fields.map do |to, from|
+          eq_conditions = paired_join_fields.map do |to, from|
             table[to].eq(record[from])
           end
           Arel::Nodes::And.new(eq_conditions)
@@ -23,6 +25,13 @@ module ActiveRecord
         end
 
         condition
+      end
+
+      def through_records_for(record)
+        association_fk, association_pk = construct_join_fields
+        record_attributes = record.attributes.slice(*association_pk)
+        candidates = Array.wrap(through_association.target)
+        candidates.find_all { |c| c.attributes.slice(*association_fk) == record_attributes }
       end
     end
   end
